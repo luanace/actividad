66f5094c11e6eb1d88070787398a86ee7596b854
luanace <153952112+luanace@users.noreply.github.com>
2023-12-15 17:27:22 -0300

    Add files via upload

---
+++ Producto/src/ListaProducto.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+public class ListaProducto {
+	
+	ArrayList producto ; public ListaProducto(  ) {
+		lista.add("Codigo");
+		lista.add("Nombre");
+		lista.add("Costo");
+
+	}
+	
+ArrayList <String> listaproductos = new ArrayList<String>();
+
+	 String elementos = lista.get();
+	 
+}
+ 
---
+++ Producto/src/lista.java
@@ -0,0 +1,15 @@
+
+public enum lista {
+	;
+
+	public static String get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void add(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
